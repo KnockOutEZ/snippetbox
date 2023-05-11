6aab0286817681456daf739436e9d46896983cfc
towhid <ktowhid20@gmail.com>
2023-05-12 00:37:25 +0600

    finished setting up cli flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"strconv"
 )
 
+type config struct {
+	addr      uint64
+	staticDir string
+}
+
 func main() {
+	var cfg config
+	flag.Uint64Var(&cfg.addr, "addr", 4000, "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Print("Starting server on http://localhost:4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on http://localhost:%d", cfg.addr)
+	err := http.ListenAndServe(":"+strconv.FormatUint(cfg.addr, 10), mux)
 	log.Fatal(err)
 }
