56ea08019e66f8cc900bf82376590ddd5d93c688
towhid <ktowhid20@gmail.com>
2023-05-24 13:57:13 +0600

    added a common dynamic data. In this case we added current year in our footer

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"html/template"
+	"net/http"
 	"path/filepath"
+	"time"
 
 	"github.com/nexentra/snippetbox/internal/models"
 )
 
 type templateData struct {
+	CurrentYear int
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
 }
+
+func (app *Application) newTemplateData(r *http.Request) *templateData {
+	return &templateData{
+	CurrentYear: time.Now().Year(),
+	}
+	}
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
