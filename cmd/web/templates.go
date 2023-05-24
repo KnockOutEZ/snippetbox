221487b080e6ff4df54c650a097543a823a84574
towhid <ktowhid20@gmail.com>
2023-05-24 14:05:47 +0600

    custom template functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -11,15 +11,23 @@ import (
 
 type templateData struct {
 	CurrentYear int
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func (app *Application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-	CurrentYear: time.Now().Year(),
+		CurrentYear: time.Now().Year(),
 	}
-	}
+}
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
@@ -29,7 +37,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	}
 	for _, page := range pages {
 		name := filepath.Base(page)
-		ts, err := template.ParseFiles("./ui/html/base.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}
