0dac06e0e79494680c46bad36f08ab1917c5217f
towhid <ktowhid20@gmail.com>
2023-05-29 01:48:45 +0600

    finished adding security policy header middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,12 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
-func (app *Application) routes(mux *http.ServeMux) *http.ServeMux {
+func (app *Application) routes(mux *http.ServeMux) http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	return mux
+	return secureHeaders(mux)
 }
