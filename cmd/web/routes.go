104b0fad5c9c724a1c3900edc1921af75da96283
towhid <ktowhid20@gmail.com>
2023-05-29 02:47:53 +0600

    added panic recovery middleware for recovering the handler panics

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,5 +10,5 @@ func (app *Application) routes(mux *http.ServeMux) http.Handler {
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	return app.logRequest(secureHeaders(mux))
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
